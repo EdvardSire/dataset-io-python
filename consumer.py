3420a32ec500174d8d177b069c6b6f678783fb1b
Edvard Sire <mail@edvardsire.com>
2025-11-13 07:18:14 +0100

    refactor

--- consumer.py
+++ consumer.py
@@ -1,13 +1,11 @@
 from pathlib import Path
 import cv2
-from server import ShmDatasetConsumer
+from dataset_io import ShmDatasetConsumer
 
 
 if __name__ == "__main__":
     sdc = ShmDatasetConsumer(Path("/home/user/datasets/UAV_VisLoc_dataset/04/drone"))
-
     for i in range(10):
         cv2.imshow("", sdc.data[i])
         cv2.waitKey(0)
-
     sdc.cleanup()
